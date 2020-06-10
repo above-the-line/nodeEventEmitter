ddfdfe8c3cf5e654120f44ec95048b8b6fb1fc27
Avi Michael <play.right.productions@gmail.com>
2020-06-10 19:11:04 +1000

    first commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+/*
+Send to the cloud every n minutes the following JSON message
+
+{
+    deviceID: ,
+    time-UTC: ,
+    kWh-exported: ,
+    kWh-imported: ,
+    actual-state: ,
+    optimal-state: ,
+}
+
+*/
